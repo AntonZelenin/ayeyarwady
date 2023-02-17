4820a5f367683924af948fa2f3e2526a64e1cf63
Anton Zelenin <ntnzelenin@gmail.com>
2023-02-18 01:22:40 +0200

    Update readme, add args to main

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
+### Description
 Creating a simple C-like programming language for fun, with a strange name Ayeyarwady.
 
-First steps are taken from [here](https://medium.com/@marcelogdeandrade/writing-your-own-programming-language-and-compiler-with-python-a468970ae6df)
+First steps are taken from [here](https://medium.com/@marcelogdeandrade/writing-your-own-programming-language-and-compiler-with-python-a468970ae6df).
 
-Requirements:
+### Requirements:
 
-Anaconda
+- Anaconda
+- LLC
+- GCC
 ```bash
 $ conda install --channel=numba llvmlite
 $ conda install -c conda-forge rply
 ```
-LLC
-
-GCC
 
 To run:
-
 ```bash
     python3 main.py
     llc -filetype=obj build/output.ll
@@ -22,7 +21,7 @@ To run:
     ./build/output
 ```
 
-Or simply 
+Or simply: 
 ```bash
 ./run.sh
 ```
--- aye/main.py
+++ aye/main.py
@@ -1,12 +1,15 @@
+import argparse
 import pathlib
 
 from lexer import Lexer
 from aye_parser import Parser
 from codegen import CodeGen
 
+DEFAULT_FILE = 'code_examples/input.aye'
 
-def main():
-    text_input = pathlib.Path('code_examples/input.aye').read_text()
+
+def main(file: str = None):
+    text_input = pathlib.Path(file or DEFAULT_FILE).read_text()
 
     lexer = Lexer().get_lexer()
     tokens = lexer.lex(text_input)
@@ -19,12 +22,16 @@ def main():
 
     pg = Parser(module, builder, printf)
     pg.parse()
-    parser = pg.get_parser()
-    parser.parse(tokens).eval()
+    parser_ = pg.get_parser()
+    parser_.parse(tokens).eval()
 
     codegen.create_ir()
     codegen.save_ir("build/output.ll")
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file')
+    args_ = parser.parse_args()
+
+    main(args_.file)
